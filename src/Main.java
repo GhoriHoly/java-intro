a603608efb72f2ac112abaaeab89f0cb150c24af
GhoriHoly <ghoriholy@gmail.com>
2024-10-30 13:10:16 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String text = "Hej";
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+
+        System.out.printf("Hello and welcome!" + text);
+
+    }
+}
